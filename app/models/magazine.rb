8f6ea02c7b3a56d88247695f125e7e5ba4b1e60f
Stephen Crane <scrane88@gmail.com>
2021-08-31 14:09:30 -0400

    done

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,3 +1,18 @@
 class Magazine < ActiveRecord::Base
-  
+  has_many :subscriptions
+  has_many :readers, through: :subscriptions
+
+  def email_list
+    reader_list = readers.pluck(:email)
+    reader_list.join(";")
+  end
+
+  def subscriber_count
+    subscriptions.count
+  end
+
+  def self.most_popular
+    highest_sub_count = all.map(&:subscriber_count).max
+    all.find { |mag| mag.subscriber_count == highest_sub_count }
+  end
 end
